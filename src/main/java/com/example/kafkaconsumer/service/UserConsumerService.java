30bb841413fd047dbfc1a2d26acdd30fab280db5
Saviour1997 <igor.bobrenok.97@gmail.com>
2022-05-17 13:36:57 +0300

    make it work correctly. problem was with converting String type

--- src/main/java/com/example/kafkaconsumer/service/UserConsumerService.java
+++ src/main/java/com/example/kafkaconsumer/service/UserConsumerService.java
@@ -2,8 +2,9 @@ package com.example.kafkaconsumer.service;
 
 import com.example.kafkaconsumer.dto.UserDto;
 import com.example.kafkaconsumer.entity.User;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public interface UserConsumerService {
 
-    User consumeData(User user);
+    void consumeData(ConsumerRecord<Long, UserDto> consumerRecord);
 }
--- src/main/java/com/example/kafkaconsumer/service/impl/UserConsumerServiceImpl.java
+++ src/main/java/com/example/kafkaconsumer/service/impl/UserConsumerServiceImpl.java
@@ -5,19 +5,23 @@ import com.example.kafkaconsumer.dto.mapper.UserMapper;
 import com.example.kafkaconsumer.entity.User;
 import com.example.kafkaconsumer.service.UserConsumerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class UserConsumerServiceImpl implements UserConsumerService {
 
-    private final UserMapper userMapper;
+//    private final UserMapper userMapper;
 
     @Override
-    @KafkaListener(topics = "users")
-    public User consumeData(User user) {
-        System.out.println("Consuming users: " + user);
-        return user;
+    @KafkaListener(topics = "users",groupId = "app.basket")
+    public void consumeData(ConsumerRecord<Long, UserDto> consumerRecord) {
+        log.info("message get, partition={}, offset={}", consumerRecord.partition(), consumerRecord.offset());
+        log.info("message get, key={}, value={}", consumerRecord.key(), consumerRecord.value());
+
     }
 }
