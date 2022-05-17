5b7d471c052b567eb428e71650ff2b76a90135d7
Saviour1997 <igor.bobrenok.97@gmail.com>
2022-05-17 13:51:16 +0300

    add MongoDb

--- src/main/java/com/example/kafkaconsumer/entity/User.java
+++ src/main/java/com/example/kafkaconsumer/entity/User.java
@@ -4,14 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Document
 public class User {
 
-    private Long id;
+    @Id
+    private String id;
 
     private String firstName;
 
--- src/main/java/com/example/kafkaconsumer/service/impl/UserConsumerServiceImpl.java
+++ src/main/java/com/example/kafkaconsumer/service/impl/UserConsumerServiceImpl.java
@@ -4,9 +4,11 @@ import com.example.kafkaconsumer.dto.UserDto;
 import com.example.kafkaconsumer.dto.mapper.UserMapper;
 import com.example.kafkaconsumer.entity.User;
 import com.example.kafkaconsumer.service.UserConsumerService;
+import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -16,12 +18,15 @@ import org.springframework.stereotype.Service;
 public class UserConsumerServiceImpl implements UserConsumerService {
 
 //    private final UserMapper userMapper;
+    private final Gson gson;
+    private final MongoTemplate mongoTemplate;
 
     @Override
     @KafkaListener(topics = "users",groupId = "app.basket")
     public void consumeData(ConsumerRecord<Long, UserDto> consumerRecord) {
         log.info("message get, partition={}, offset={}", consumerRecord.partition(), consumerRecord.offset());
         log.info("message get, key={}, value={}", consumerRecord.key(), consumerRecord.value());
-
+        User user = gson.fromJson(String.valueOf(consumerRecord.value()), User.class);
+        mongoTemplate.insert(user);
     }
 }
